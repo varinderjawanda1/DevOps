113c222703cd09ec8036e0faff67b65978eaa84d
varinderjawanda1 <jawandavs1@yahoo.com>
2020-07-21 20:12:20 -0400

    Adding tfstate, tfvars and variable files

--- instance_provisioning/new-aws-instance.tf
+++ instance_provisioning/new-aws-instance.tf
@@ -3,7 +3,14 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "jawanda" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "pwd && ls > output.txt"
+  }
+}
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = aws_instance.jawanda.id
 }
