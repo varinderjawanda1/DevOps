6d2cf5fd94ad0d0c6685b774722132ff497368ed
varinderjawanda1 <jawandavs1@yahoo.com>
2020-08-02 18:58:14 -0400

    Group, Role & Policy

--- instance_provisioning/iam.tf
+++ instance_provisioning/iam.tf
@@ -1,12 +1,24 @@
-resource "aws_iam_group" "developer" {
-  name = "dev"
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
 }
 
-resource "aws_iam_role" "developer" {
+// Dev group
+resource "aws_iam_group" "dev_group" {
+  name = "devg"
+  path = "/"
+}
+// dev role
+resource "aws_iam_role" "developer_role" {
+  name = "devr"
   assume_role_policy = ""
+  tags = {
+    tag-key = "dev_role"
 }
-resource "aws_iam_group_membership" "developer" {
-  group = "${aws_iam_group.developer.name}"
-  name = "developer_membership"
-  users = []
 }
+// dev policy attachment.
+//resource "aws_iam_role_policy_attachment" "dev_policy" {
+//  policy_arn = "arn:aws:iam::919279782144:policy/EKS_developer_read"
+//  policy_arn = "arn:aws:iam::919279782144:policy/s3_developer"
+//  role = "${aws_iam_group.dev_group.name}"
+//}
